f97f49ac9b76e8840115eba0fb92d889edf683c4
devicaro16 <icaroip15@gmail.com>
2026-06-06 12:31:35 -0300

    feat(cli): wire up app entry point

---
+++ packages/cli/src/index.tsx
@@ -0,0 +1,16 @@
+import { createCliRenderer } from "@opentui/core";
+import { createRoot } from "@opentui/react";
+import Header from "./components/Header";
+import StatusBar from "./components/status-bar";
+
+function App() {
+  return (
+    <box alignItems="center" justifyContent="center" backgroundColor="#0D0D12" width="100%" height="100%" gap={2}>
+      <Header />
+      <StatusBar />
+    </box>
+  );
+}
+
+const renderer = await createCliRenderer();
+createRoot(renderer).render(<App />);
